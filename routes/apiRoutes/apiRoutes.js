7f67b20c0d08c31f92f203c78d56bac993d04086
tbrooks <tbrooks321@yahoo.com>
2022-02-10 19:49:36 -0500

    more work on api routes

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -4,9 +4,15 @@ const fs = require('fs');
 const uuid = require('uuid');
 
 
-
 router.get('/notes', (req, res) => {
    // get all note from database that exist 
+   fs.readFile('../../db/db.json', 'utf8')
+   .then(data => {
+      return res.json(data)
+   })
+   .then(notes => console.log(notes))
+   .catch(err => 
+    res.status(500).json(err));
 });
 
 router.post('/notes', (req, res) => {
--- server.js
+++ server.js
@@ -1,24 +1,16 @@
 const express = require('express');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes');
 
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`Now listening on ${PORT}`)
